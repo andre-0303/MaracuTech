c40fbefcd61cb64a3e1d23527649cb2ec1338445
Bandeira <bandeiradev.ti@gmail.com>
2026-02-01 19:28:17 -0300

    fix(talhao): adicionar ListTalhoesUseCase e GetTalhaoByIdUseCase ao module
    - Corrige erro de dependências no TalhoesResolver
    - Registra todos os use cases no providers do TalhoesModule

--- src/domain/talhao/talhoes.module.ts
+++ src/domain/talhao/talhoes.module.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TALHOES_REPOSITORY } from './domain/repositories/talhoes-repository.token';
 import { TypeOrmTalhoesRepository } from './domain/repositories/typeorm-talhoes.repository';
 import { CreateTalhaoUseCase } from './application/use-cases/create-talhao/create-talhao.use-case';
+import { ListTalhoesUseCase } from './application/use-cases/list-talhoes/list-talhoes.use-case';
+import { GetTalhaoByIdUseCase } from './application/use-cases/get-talhao-by-id/get-talhao-by-id.use-case';
 import { TalhoesResolver } from './infra/graphql/talhoes.resolver';
 import { TalhaoOrmEntity } from './infra/typeorm/entities/talhao.orm-entity';
 import { PlantioOrmEntity } from './infra/typeorm/entities/plantio.orm-entity';
@@ -20,6 +22,8 @@ import { ColheitaOrmEntity } from './infra/typeorm/entities/colheita.orm-entity'
   providers: [
     TalhoesResolver,
     CreateTalhaoUseCase,
+    ListTalhoesUseCase,
+    GetTalhaoByIdUseCase,
     {
       provide: TALHOES_REPOSITORY,
       useClass: TypeOrmTalhoesRepository,
